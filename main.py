7223db0b4b06a78a8a27bbc9d5d7c15a9fc58917
Swaraj Prasad Panchal <85017678+AndreW13-sp@users.noreply.github.com>
2023-12-26 22:52:44 +0530

    Updated excel file

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import time
 from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import Workbook
 
-print("Starting the automated test case...")
+
+def write_to_excel(test_case, description, expected_output, result):
+    wb = Workbook()
+    sheet = wb.active
+    sheet.append(["Test Case", "Description", "Expected Output", "Pass/Fail"])
+    sheet.append([test_case, description, expected_output, result])
+    wb.save("test_results.xlsx")
+
+
+print("Sample test case started")
 
 # Initialize Chrome WebDriver
 driver = webdriver.Chrome()
 driver.maximize_window()
 
 try:
-    print("Navigating to the W3Schools login page...")
     # Open the W3Schools login page
     driver.get("https://profile.w3schools.com/")
 
     # Wait for the login page to load
-    print("Waiting for the login page to load...")
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
 
-    print("Entering login credentials...")
     # Enter login credentials
     email_input = driver.find_element(By.NAME, "email")
     email_input.send_keys("dropbox6393@gmail.com")
@@ -28,23 +34,23 @@ try:
     password_input = driver.find_element(By.NAME, "current-password")
     password_input.send_keys("PassWord@123")
 
-    print("Submitting the login form...")
     # Find and click the submit button using class name
     submit_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CLASS_NAME, "Button_primary__d2Jt3"))
     )
     submit_button.click()
 
-    print("Waiting for the redirect after login...")
     # Wait for the redirect to the dashboard after login
     redirect_url = "https://my-learning.w3schools.com/"
     WebDriverWait(driver, 10).until(EC.url_contains(redirect_url))
 
-    # Check if redirected to the expected page after login
+    # Test Case 1: Redirected to the dashboard after successful login
     if redirect_url in driver.current_url:
-        print("Successfully logged in! Redirected to the dashboard.")
+        print("Redirected to the dashboard after successful login")
+        write_to_excel("TC1", "Login to W3Schools profile", "Redirected to dashboard", "Pass")
     else:
         print("Unexpected redirect or login failed. Current URL:", driver.current_url)
+        write_to_excel("TC1", "Login to W3Schools profile", "Redirected to dashboard", "Fail")
 
 except TimeoutException as e:
     print("TimeoutException occurred:", e)
@@ -55,5 +61,4 @@ except Exception as ex:
 
 finally:
     # Quit the WebDriver session
-    print("Quitting the WebDriver session...")
     driver.quit()
